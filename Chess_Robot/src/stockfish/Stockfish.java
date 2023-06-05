f8ea62be4738cf03db3ce31f0fd24812f36a4255
SungyeopJeong <syeopj4a@gmail.com>
2023-06-06 01:22:58 +0900

    style: Format stockfish.java

--- Chess_Robot/src/stockfish/Stockfish.java
+++ Chess_Robot/src/stockfish/Stockfish.java
@@ -6,25 +6,27 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Stockfish {
-	
+
 	private Process engineProcess;
 	private BufferedReader processReader;
 	private OutputStreamWriter processWriter;
-	
+
 	private static final String PATH = "./engine/stockfish.exe";
-	
+
 	public boolean startEngine() {
 		try {
 			engineProcess = Runtime.getRuntime().exec(PATH);
-			processReader = new BufferedReader(new InputStreamReader(engineProcess.getInputStream()));
-			processWriter = new OutputStreamWriter(engineProcess.getOutputStream());
+			processReader = new BufferedReader(new InputStreamReader(
+					engineProcess.getInputStream()));
+			processWriter = new OutputStreamWriter(
+					engineProcess.getOutputStream());
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-	
+
 	public void stopEngine() {
 		try {
 			sendCommand("quit");
@@ -34,7 +36,7 @@ public class Stockfish {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void sendCommand(String command) {
 		try {
 			processWriter.write(command + "\n");
@@ -43,41 +45,43 @@ public class Stockfish {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public String getOutput(int waitTime) {
 		StringBuffer buffer = new StringBuffer();
-		
+
 		try {
 			Thread.sleep(waitTime);
 			sendCommand("isready");
 			while (true) {
 				String output = processReader.readLine();
-				if (output.equals("readyok")) break;
-				else buffer.append(output + "\n");
+				if (output.equals("readyok"))
+					break;
+				else
+					buffer.append(output + "\n");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return buffer.toString();
 	}
-	
+
 	public String getBestMove(String fen, int waitTime) {
 		sendCommand("position fen " + fen);
 		sendCommand("go movetime " + waitTime);
 		return getOutput(waitTime).split("bestmove ")[1].split(" ")[0];
 	}
-	
+
 	public String getLegalMoves(String fen) {
 		sendCommand("position fen " + fen);
 		sendCommand("d");
 		return getOutput(0).split("Legal moves: ")[1];
 	}
-	
+
 	public void drawBoard(String fen) {
 		sendCommand("position fen " + fen);
 		sendCommand("d");
 		String[] rows = getOutput(0).split("\n");
-		
+
 		for (int i = 1; i < 18; i++) {
 			System.out.println(rows[i]);
 		}
